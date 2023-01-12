dd4b001f15a5e6258000d66fccb3bb66712f43d9
colored-dye <baoyuntai@outlook.com>
2023-01-12 17:03:15 +0800

    LLVM-MinGW example with checksum

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,5 @@
 add_rules("mode.debug")
+add_requires("llvm-mingw")
 
 target("dpp-example")
     add_rules("wdk.driver", "wdk.env.wdm")
@@ -9,3 +10,22 @@ target("loader")
     add_rules("wdk.binary", "wdk.env.wdm")
     set_values("wdk.env.winver", "win7")
     add_files("exe/*.c")
+
+target("llvm-mingw-test")
+    add_deps("checksum")
+    set_kind("binary")
+    set_toolchains("mingw@llvm-mingw")
+    add_files("llvm-mingw-test/*.c")
+    add_links("ntdll")
+    add_rules("correct_checksum")
+
+target("checksum")
+    set_kind("binary")
+    add_files("checksum/*.c")
+
+rule("correct_checksum")
+    after_build(
+        function (target)
+            print("Correct `%s''s checksum", target:targetfile())
+        end
+    )
